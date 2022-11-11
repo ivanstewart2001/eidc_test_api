7d39893d8c4ba7f68a2fe4a3d1ab79d636697f26
ivanstewart2001 <geoffreystewart2001@gmail.com>
2022-11-11 17:20:27 -0500

    forgot to build

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const PORT = process.env.PORT || 4000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -23,6 +25,6 @@ async function bootstrap() {
   // await app.listen(3000);
 
   app.enableCors(); // protection
-  await app.listen(3002)
+  await app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 }
 bootstrap();
